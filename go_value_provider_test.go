ed12d4199036c55638427bb15f1e5fc97bbdbfc0
Bartosz Ciulkin <bartosz.ciulkin@gmail.com>
2024-10-09 21:57:38 +0200

    Add panic test

--- go_value_provider_test.go
+++ go_value_provider_test.go
@@ -14,8 +14,19 @@ func TestInt(t *testing.T) {
 }
 
 func TestUpperLimitedInt(t *testing.T) {
-  result := UpperLimitedInt(123)
+ 
+  t.Run("successful", func(t *testing.T) {
+    result := UpperLimitedInt(123)
 
-  assert.LessOrEqual(t, result, 123, "Result should be less or equal than limited value")
-  assert.GreaterOrEqual(t, result, math.MinInt, "Result should be greater or equal than minInt")
+    assert.LessOrEqual(t, result, 123, "Result should be less or equal than limited value")
+    assert.GreaterOrEqual(t, result, math.MinInt, "Result should be greater or equal than minInt")
+  })
+
+  t.Run("failure", func(t *testing.T) {
+    assert.Panics(t, func() { UpperLimitedInt(0) }, "Code did not panic.")
+  })
+
+
+//  assert.LessOrEqual(t, result, 123, "Result should be less or equal than limited value")
+//  assert.GreaterOrEqual(t, result, math.MinInt, "Result should be greater or equal than minInt")
 }
